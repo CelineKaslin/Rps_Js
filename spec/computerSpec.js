6b0e00196ee783ccbb6174aeb2e648f4de00dd86
celine kaslin <celine.kaslin@gmail.com>
2019-01-18 15:56:12 +0000

    test added for computer class

--- spec/computerSpec.js
+++ spec/computerSpec.js
@@ -6,10 +6,18 @@ describe ("Computer spied to return one of the random value it should play", fun
   });
 
   it ("The choice of the computer should return 'Rock'", function(){
-    spyOn(Math, 'random').and.returnValue(0);
+    spyOn(Math, 'floor').and.returnValue(0);
     expect(computer.play()).toEqual('Rock');
   });
 
+  it ("The choice of the computer should return 'Scissors'", function(){
+    spyOn(Math, 'floor').and.returnValue(1);
+    expect(computer.play()).toEqual('Scissors');
+  })
 
+  it ("The choice of the computer should return 'Paper'", function(){
+    spyOn(Math, 'floor').and.returnValue(2);
+    expect(computer.play()).toEqual('Paper');
+  });
 
 });
--- src/computer.js
+++ src/computer.js
@@ -2,9 +2,9 @@ class Computer {
 
   play() {
     let choices = ['Rock', 'Scissors', 'Paper']
+    console.log(choices.length);
     let randomChoice = Math.floor(Math.random() * choices.length);
     let randomPlay = choices[randomChoice];
     return randomPlay;
   };
-
 };
