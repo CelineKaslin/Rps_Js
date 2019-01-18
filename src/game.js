d7217d39ffbb980118d00635a63021ea5dbda8d8
celine kaslin <celine.kaslin@gmail.com>
2019-01-18 10:00:59 +0000

    test passing for all rock funcion

--- src/game.js
+++ src/game.js
@@ -3,6 +3,8 @@ class Game {
   rock(computer_choice) {
     switch (computer_choice){
       case ('Rock'): return 'Oups, it is a draw!'
+      case ('Scissors'): return 'Well done, you won!'
+      case ('Paper'): return 'Sadly, you loose!'
     };
   };
 };
